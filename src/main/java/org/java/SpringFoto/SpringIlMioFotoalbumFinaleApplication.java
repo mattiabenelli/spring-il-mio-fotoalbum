f4604e4af90ce80630f7d2c93ef0f6c97f7c2e65
mattiabenelli <benellimattia0@gmail.com>
2023-06-09 12:23:59 +0200

    setup commit

---
+++ src/main/java/org/java/SpringFoto/SpringIlMioFotoalbumFinaleApplication.java
@@ -0,0 +1,15 @@
+package org.java.SpringFoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIlMioFotoalbumFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIlMioFotoalbumFinaleApplication.class, args);
+		
+		System.out.println("helloworld");
+	}
+
+}
---
+++ src/test/java/org/java/SpringFoto/SpringIlMioFotoalbumFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.SpringFoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIlMioFotoalbumFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
