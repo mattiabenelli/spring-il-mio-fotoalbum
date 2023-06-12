fc8393537667567657209b1a21479d1af4709b0c
mattiabenelli <benellimattia0@gmail.com>
2023-06-12 09:37:52 +0200

    collegamento api front back

--- front-office-album/src/views/HomeView.vue
+++ front-office-album/src/views/HomeView.vue
@@ -1,7 +1,34 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+<script>
+const BASE_API_URL = "http://localhost:8080/api/v1";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      foto: [],
+    };
+  },
+  methods: {
+    getImage() {
+      axios
+        .get(BASE_API_URL + "/foto")
+        .then((res) => {
+          this.foto = res.data;
+          console.log(this.foto);
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.getImage();
+  },
+};
 </script>
 
 <template>
-  <main>helloworld</main>
+  <main>
+    <ul>
+      <li v-for="item in foto" :key="item.id">{{ item.titolo }}</li>
+    </ul>
+  </main>
 </template>
